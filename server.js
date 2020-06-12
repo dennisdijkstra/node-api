6f07fd3c87a3874f243504c494cdbc2db21411e0
dennisdijkstra <mail@dennisdijkstra.com>
2020-06-12 22:27:57 +0200

    handle unhandled promise rejections

--- server.js
+++ server.js
@@ -23,7 +23,13 @@ if (process.env.BODE_ENV = 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
 
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.bold));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server and exit process
+    server.close(() => process.exit(1));
+})
