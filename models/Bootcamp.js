057386331f843dc8925e97e2b032dcdb736c1010
“Dennis <dennis@socialseeder.com>
2020-06-14 17:14:20 +0200

    handle GET and POST requests in bootcamp controller

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const BootcampSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Please add a name'].
+        required: [true, 'Please add a name'],
         unique: true,
         trim: true,
         maxLength: [50, 'Name can not be more than 50 characters'],
@@ -11,7 +11,7 @@ const BootcampSchema = new mongoose.Schema({
     slug: String,
     description: {
         type: String,
-        required: [true, 'Please add a description'].
+        required: [true, 'Please add a description'],
         maxLength: [500, 'Description can not be more than 500 characters'],
     },
     website: {
