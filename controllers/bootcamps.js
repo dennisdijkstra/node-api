9775d8a2216f7f589e3b0005e3054c1bf9dfebca
“Dennis <mail@dennisdijkstra.com>
2020-07-23 15:38:18 +0200

    install and use express fileupload

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -147,3 +147,22 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
         data: {},
     });
 });
+
+// @desc        Upload photo for bootcamp
+// @routes      PUT /api/v1/bootcamps/:id/photo
+// @access      Private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if(!bootcamp) {
+        return next(
+            new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+        );
+    }
+
+    if(!req.files) {
+        return next(
+            new ErrorResponse(`Please upload a file`, 404)
+        );
+    }
+});
