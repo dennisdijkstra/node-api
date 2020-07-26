2f45423f743adbc739c358b039ead5964b57c045
“Dennis <mail@dennisdijkstra.com>
2020-07-26 21:02:24 +0200

    create route and controller for authentication

--- models/User.js
+++ models/User.js
@@ -28,7 +28,7 @@ const UserSchema = new mongoose.Schema({
     resetPasswordToken: String,
     resetPasswordExpire: Date,
     createdAt: {
-        type: Data,
+        type: Date,
         default: Date.now,
     },
 });
