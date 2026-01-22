376373e80bba65cd3e43a9b12a34a7fdd41aa4d1
Dennis Dijkstra <mail@dennisdijkstra.com>
2026-01-22 14:47:02 +0100

    Tweak indentation

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -17,84 +17,84 @@ const BootcampSchema = new mongoose.Schema({
         maxLength: [500, 'Description can not be more than 500 characters'],
     },
     website: {
-      type: String,
-      match: [
-        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-        'Please use a valid URL with HTTP or HTTPS'
-      ]
+        type: String,
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+            'Please use a valid URL with HTTP or HTTPS'
+        ]
     },
     phone: {
-      type: String,
-      maxlength: [20, 'Phone number can not be longer than 20 characters']
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters']
     },
     email: {
-      type: String,
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        'Please add a valid email'
-      ]
+        type: String,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
     },
     address: {
-      type: String,
-      required: [true, 'Please add an address']
+        type: String,
+        required: [true, 'Please add an address']
     },
     location: {
-      type: {
-        type: String,
-        enum: ['Point']
-      },
-      coordinates: {
-        type: [Number],
-        index: '2dsphere'
-      },
-      formattedAddress: String,
-      street: String,
-      city: String,
-      state: String,
-      zipcode: String,
-      country: String
+        type: {
+            type: String,
+            enum: ['Point']
+        },
+        coordinates: {
+            type: [Number],
+            index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String
     },
     careers: {
-      type: [String],
-      required: true,
-      enum: [
-        'Web Development',
-        'Mobile Development',
-        'UI/UX',
-        'Data Science',
-        'Business',
-        'Other'
-      ]
+        type: [String],
+        required: true,
+        enum: [
+            'Web Development',
+            'Mobile Development',
+            'UI/UX',
+            'Data Science',
+            'Business',
+            'Other'
+        ]
     },
     averageRating: {
-      type: Number,
-      min: [1, 'Rating must be at least 1'],
-      max: [10, 'Rating must can not be more than 10']
+        type: Number,
+        min: [1, 'Rating must be at least 1'],
+        max: [10, 'Rating must can not be more than 10']
     },
     averageCost: Number,
     photo: {
-      type: String,
-      default: 'no-photo.jpg'
+        type: String,
+        default: 'no-photo.jpg'
     },
     housing: {
-      type: Boolean,
-      default: false
+        type: Boolean,
+        default: false
     },
     jobAssistance: {
-      type: Boolean,
-      default: false
+        type: Boolean,
+        default: false
     },
     jobGuarantee: {
-      type: Boolean,
-      default: false
+        type: Boolean,
+        default: false
     },
     acceptGi: {
-      type: Boolean,
-      default: false
+        type: Boolean,
+        default: false
     },
     createdAt: {
-      type: Date,
-      default: Date.now
+        type: Date,
+        default: Date.now
     }
 }, {
     toJSON: { virtuals: true },
